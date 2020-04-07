b958463804f708a08064a25a9b214ad3afed4a94
Sam Kamenetz <samkamenetz@gmail.com>
2020-04-07 14:52:16 -0400

    troubleshooting: start here

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const keys = require("./config/keys");
 const express = require("express");
+const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport')
 
 const app = express();
 require('./routes/authRoutes')(app);
 
-
+mongoose.connect(keys.mongoURI);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there friend" });
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require('mongoose');
 
+const User = mongoose.model('users');
 passport.use(
     new GoogleStrategy(
       {
@@ -10,9 +12,18 @@ passport.use(
         callbackURL: "/auth/google/callback",
       },
       (accessToken, refreshToken, profile, done) => {
-        console.log("accessToken", accessToken);
-        console.log("refresh token", refreshToken);
-        console.log("profile", profile);
+        console.log("profile", profile.id);
+        var user = User({ googleId: profile.id});
+        user.save();
+        console.log(user);
+
+        User.find({}, function(err, users){
+          if (err) throw err;
+          console.log(JSON.stringify(users, null, 4));
+        });
+        //console.log("accessToken", accessToken);
+        //console.log("refresh token", refreshToken);
+
       }
     )
   );
